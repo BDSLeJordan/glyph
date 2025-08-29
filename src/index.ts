92d49a6bb8fb414b901df4445babc7db32a8e9bf
BDSLeJordan <jojos60000@hotmail.fr>
2025-08-29 18:09:59 +0200

    refactor(index): switch to named export only; remove default export for Glyph

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
-import { Glyph } from "./classes/Glyph";
+export { Glyph } from "./classes/Glyph";
 
 export * from "./types";
 
-export default Glyph;
