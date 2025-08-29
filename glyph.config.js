4c15420511cfa293d1c05aa763e9f5f4ca85da30
BDSLeJordan <jojos60000@hotmail.fr>
2025-08-29 16:28:05 +0200

    chore(config): simplify default glyph.config.js

--- glyph.config.js
+++ glyph.config.js
@@ -1,9 +1,7 @@
-import dotenv from "dotenv";
-dotenv.config();
 const config = {
 	emojisDir: "emojis",
 	fileIndex: true,
-	botToken: process.env.BOT_TOKEN,
+	botToken,
 };
 
 export default config;
